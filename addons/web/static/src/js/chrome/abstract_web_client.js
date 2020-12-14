5fe7f44e04046c37a2942864071c6673db369919
Ashwin <ashwinpradhan@bajratechnologies.com>
2020-12-14 15:16:20 +0545

    added ciwec branding

--- addons/web/static/src/js/chrome/abstract_web_client.js
+++ addons/web/static/src/js/chrome/abstract_web_client.js
@@ -101,7 +101,7 @@ var AbstractWebClient = Widget.extend(ServiceProviderMixin, KeyboardNavigationMi
         this._current_state = null;
         this.menu_dp = new concurrency.DropPrevious();
         this.action_mutex = new concurrency.Mutex();
-        this.set('title_part', {"zopenerp": "BBH's HealthSuite"});
+        this.set('title_part', {"zopenerp": "CIWEC's HealthSuite"});
     },
     start: function () {
         var self = this;
